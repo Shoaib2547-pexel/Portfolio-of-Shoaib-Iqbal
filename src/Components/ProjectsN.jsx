a9bf1812fa01d2b658865e768f31b5c6a710a37b
Shoaib Iqbal <shoaibiqbal2547@gmail.com>
2025-08-17 22:35:17 -0400

    Pic login

--- src/Components/ProjectsN.jsx
+++ src/Components/ProjectsN.jsx
@@ -1,25 +1,56 @@
 import React, { useState } from 'react'
-import profile from '../Images/travel.png'
 import proImg from '../Images/proImg.jpg'
+import loginPic from '../Images/login.jpg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSquareArrowUpRight, faUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
 
 export default function ProjectsN() {
     let [hovereffect, sethovereffect] = useState(null)
 
-    let objects = [1, 2, 3, 4, 5, 6]
+    let objects = [
+        {
+            img: loginPic,
+            heading: 'Login Page',
+            writing: 'Animated Sign Up and Login Page'
+        },
+        {
+            img: proImg,
+            heading: 'Portfolio',
+            writing: 'Created to display creativity of Shoaib Iqbal'
+        },
+        {
+            img: proImg,
+            heading: 'Portfolio',
+            writing: 'Created to display creativity of Shoaib Iqbal'
+        },
+        {
+            img: proImg,
+            heading: 'Portfolio',
+            writing: 'Created to display creativity of Shoaib Iqbal'
+        },
+        {
+            img: proImg,
+            heading: 'Portfolio',
+            writing: 'Created to display creativity of Shoaib Iqbal'
+        },
+        {
+            img: proImg,
+            heading: 'Portfolio',
+            writing: 'Created to display creativity of Shoaib Iqbal'
+        },
+    ]
 
     return (
         <div className='grid grid-cols-1 md:grid-cols-3 gap-4 bg-black p-6 mx-auto '>
             {objects.map((items,index) => (
                 <div onMouseEnter={() => sethovereffect(index)} onMouseLeave={() => sethovereffect(null)} className='relative h-[170px] max-w-[952px] rounded-lg border-2 border-yellow-300 shadow-[0_0_10px_1px_rgba(250,204,21,0.7)] '>
-                    <img src={proImg} className='w-full h-full object-contain' />
+                    <img src={items.img} className='w-full h-full object-contain' />
                     {
                         hovereffect === index &&
                         <div className='absolute inset-0 z-50 bg-gradient-to-t from-yellow-300 to-transparent flex flex-col items-center justify-end py-3 px-2'>
                             
-                                <h1 className='text-black font-extrabold text-[20px] drop-shadow-xl'>Portfolio</h1>
-                                <p className='text-black shadow-white font-bold drop-shadow-lg'>Created to display creativity of Shoaib Iqbal</p>
+                                <h1 className='text-black font-extrabold text-[20px] drop-shadow-xl'>{items.heading}</h1>
+                                <p className='text-black shadow-white font-bold drop-shadow-lg'>{items.writing}</p>
                                <a href='https://shoaib-portfolio-phi.vercel.app/ ' target='_blank'><FontAwesomeIcon icon={faSquareArrowUpRight} className='text-black w-[40px] h-[40px]'/></a>
  
                         </div>
